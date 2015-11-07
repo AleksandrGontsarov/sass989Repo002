a26e2003496e91aff5ddbef76204766a94238113
sass989 <sass989@yahoo.com>
2015-11-07 21:07:34 +0200

    Добавлена строка с тремя точками.

--- if029.java
+++ if029.java
@@ -69,5 +69,6 @@ public class if029 {
         System.out.println("Данное число :");
         System.out.println(result);
         System.out.println(".");
+        System.out.println("...");
     }
 }
